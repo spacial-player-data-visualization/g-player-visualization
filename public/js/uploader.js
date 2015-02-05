83d216de7bd83a1d8b0932767d634fe0bf1d9b4c
Alex Johnson <alex@tablelist.com>
2015-02-05 16:16:26 -0500

    Remove empty lines from parsed data

---
+++ public/js/uploader.js
@@ -0,0 +1,29 @@
+// http://www.joyofdata.de/blog/parsing-local-csv-file-with-javascript-papa-parse/
+function parseFile(event) {
+  var file = event.target.files[0];
+
+  Papa.parse(file, {
+    header: false,
+    dynamicTyping: true,
+
+    // Parser Callback
+    complete: function(results) {
+      
+      var data = results.data;
+      var errors = results.errors;
+      var meta = results.meta;
+
+      // Remove empty lines
+      data = _.filter(data, function(dat){
+        return dat.length > 1;
+      })
+
+      console.log(data);
+    }
+  });
+}
+
+// Watch File Input
+$(document).ready(function(){
+  $("#csv-file").change(parseFile);
+});
