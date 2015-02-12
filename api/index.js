1399cba5d2d34738b02d95d0e481eb76537dc1e5
tomxhu <tomxhu@gmail.com>
2015-02-11 19:35:17 -0500

    initial CRUD implementation

--- api/index.js
+++ api/index.js
@@ -1,13 +1,109 @@
 var express = require('express');
+var connect = require('connect')
+var bodyParser = require('body-parser')
 var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var Schema = mongoose.Schema;
+
+var Entry = new Schema({
+  area: {type: String},
+  playerID: {type: Number},
+  timestamp: {type: Number},
+  posX: {type: Number},
+  posY: {type: Number},
+  cameraX: {type: Number},
+  cameraY: {type: Number}
+});
+
+var EntryModel = mongoose.model('Entry', Entry);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/api/entries', function (req, res){
+  return EntryModel.find(function (err, entries) {
+    if (err) {
+      console.log(err);
+    } else {
+      return res.send(entries);
+    }
+  });
+});
+
+app.post('/api/entries', function (req, res){
+  console.log(req.body.playerID);
+  var entry = new EntryModel({
+    area: req.body.area,
+    playerID: req.body.playerID,
+    timestamp: req.body.timestamp,
+    posX: req.body.posX,
+    posY: req.body.posY,
+    cameraX: req.body.cameraX,
+    cameraY: req.body.cameraY
+  });
+
+  entry.save(function (err){
+    if (err) {
+      console.log(err);
+    } else {
+      return console.log('saved');
+    }
+  });
+
+  return res.send(entry);
+});
+
+app.get('/api/entries/:id', function (req, res){
+  return EntryModel.findById(req.params.id , function (err, entry){
+    if (err) {
+      console.log(err);
+    } else {
+      return res.send(entry);
+    }
+  });
+});
+
+app.put('/api/entries/:id', function (req, res){
+  return EntryModel.findById(req.params.id, function (err, entry){
+    entry.area = req.body.area;
+    entry.playerID = req.body.playerID;
+    entry.timestamp = req.body.timestamp;
+    entry.posX = req.body.posX;
+    entry.posY = req.body.posY;
+    entry.cameraX = req.body.cameraX;
+    entry.cameraY = req.body.cameraY;
+    return entry.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('updated');
+      }
+      return res.send(entry);
+    });
+  })
+});
+
+app.delete('/api/products/:id', function (req, res){
+  return EntryModel.findById(req.params.id, function (err, entry){
+    return entry.remove(function (err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('deleted');
+      }
+    });
+  })
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
+
