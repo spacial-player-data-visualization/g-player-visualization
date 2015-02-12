f5ddc861fbdc4be09e032bb964aab749f1b43356
Alex Johnson <alex@tablelist.com>
2015-02-12 13:01:12 -0500

    Add JS for connecting to API

---
+++ public/js/api.js
@@ -0,0 +1,71 @@
+
+/******************************
+         api.js 
+ ******************************/
+
+var API = {
+
+  // Target API
+  url : "http://g-player.herokuapp.com/api/";
+};
+
+/******************************
+      CRUD Functionality
+ ******************************/
+
+// Reference on XHR callbacks
+// http://stackoverflow.com/questions/5485495/how-can-i-take-advantage-of-callback-functions-for-asynchronous-xmlhttprequest
+
+// Original code from
+// https://github.com/data-visualization-capstone/web/blob/development/src/js/api.js
+
+// GET
+// takes target URL, callback, callback
+API.get = function(url, success, error){
+	var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+      
+      // If call is complete & successfull
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var resp = JSON.parse(xhr.response);
+        success(resp);
+
+      } else if (xhr.readyState == 4 && xhr.status != 200) {
+        console.log("API Call Error.");
+        console.log(xhr.response);
+
+        error(xhr);
+      }
+    };
+
+    xhr.open("GET", DV.url + url, false);
+    xhr.send();
+}
+
+API.post = function(url, params, success, error){
+	var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+      
+      // If call is complete & successfull
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        success(JSON.parse(xhr.response));
+
+      } else if (xhr.readyState == 4 && xhr.status != 200) {
+        console.log("API Call Error.");
+      
+        error(xhr);
+      }
+    };
+
+	xhr.open("POST", DV.url + url, false);
+
+    // Send the proper header information along with the request
+  	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  	// xhr.setRequestHeader("Connection", "close");
+    
+    // var params = JSON.stringify(params);
+    // console.log(params);
+    xhr.send(params);
+}
