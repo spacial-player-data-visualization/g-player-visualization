eeac72f9e194349e0b48e43629a538311bd8e702
iBroadband <agimmi00@gmail.com>
2015-02-18 18:09:47 -0500

    Added Query Builder Basics
    Started working on the GUI for the Query Builder

--- public/js/query-builder.js
+++ public/js/query-builder.js
@@ -1,1 +1,19 @@
-// 
+// Show the map selector by default
+$("#groupPlayerID, #groupTimestamp").hide();
+
+// Only show relevant followup inputs for the selected option
+$(document).ready(function (){
+	$("#selectQueryType").change(function() {
+
+		var selection = $(this).val();
+		$("#groupMap, #groupPlayerID, #groupTimestamp").hide();
+
+		if (selection == "area") {
+			$("#groupMap").show();
+		} else if (selection == "playerID") {
+			$("#groupPlayerID").show();
+		} else if (selection == "timestamp") {
+			$("#groupTimestamp").show();
+		}
+	});
+});
