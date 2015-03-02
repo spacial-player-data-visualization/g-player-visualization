071b35c73a9e50e6d6db0f08abd1eb3314b330d0
Alex Johnson <alex@tablelist.com>
2015-03-01 23:06:49 -0500

    _.contains was returning false. Resort to .indexOf for URL detection

--- public/js/app.js
+++ public/js/app.js
@@ -25,9 +25,9 @@ var keyMapping =
 var environment = document.URL;
 
 var API = {
-
+	
   // Target API. Set depending on environment
-  url : _.contains(environment, "herokuapp.com") ? "http://g-player.herokuapp.com/api/" : "http://localhost:5000/api/",
+  url : (window.location.href.indexOf("herokuapp.com") > -1) ? "http://g-player.herokuapp.com/api/" : "http://localhost:5000/api/",
 };
 
 var loading = {};
