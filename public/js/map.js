7ad182fe689894c4d96ea9eeb6f8da69a7cf5df0
Alex Johnson <alex@tablelist.com>
2015-03-16 14:31:29 -0400

    Download currently active data set as a .CSV

--- public/js/map.js
+++ public/js/map.js
@@ -76,6 +76,68 @@ MAP.getData = function(){
     })
 };
 
+
+// Export currently active data set
+// as a .csv file.
+
+// http://jsfiddle.net/sturtevant/vUnF9/
+// http://stackoverflow.com/a/4130939/317
+
+MAP.exportCSV = function(){
+	var json = settings.data;
+
+	// var json = $.parseJSON(json);
+    var csv = JSON2CSV(json);
+
+   	// Trick browser to force download.
+    window.open("data:text/csv;charset=utf-8," + escape(csv))
+
+
+	function JSON2CSV(objArray) {
+	    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+
+	    var str = '';
+	    var line = '';
+
+	    if ($("#labels").is(':checked')) {
+	        var head = array[0];
+	        if ($("#quote").is(':checked')) {
+	            for (var index in array[0]) {
+	                var value = index + "";
+	                line += '"' + value.replace(/"/g, '""') + '",';
+	            }
+	        } else {
+	            for (var index in array[0]) {
+	                line += index + ',';
+	            }
+	        }
+
+	        line = line.slice(0, -1);
+	        str += line + '\r\n';
+	    }
+
+	    for (var i = 0; i < array.length; i++) {
+	        var line = '';
+
+	        if ($("#quote").is(':checked')) {
+	            for (var index in array[i]) {
+	                var value = array[i][index] + "";
+	                line += '"' + value.replace(/"/g, '""') + '",';
+	            }
+	        } else {
+	            for (var index in array[i]) {
+	                line += array[i][index] + ',';
+	            }
+	        }
+
+	        line = line.slice(0, -1);
+	        str += line + '\r\n';
+	    }
+	    return str;   
+	}
+};
+
+
 /************************************
        Heatmap Logic
 ************************************/
