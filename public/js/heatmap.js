63ab874c576c9855231b27c894e7e6646f65ff1d
Alex Johnson <alex@tablelist.com>
2015-03-29 12:53:24 -0400

    Move filters to right side. Left nav no longer collapes

--- public/js/heatmap.js
+++ public/js/heatmap.js
@@ -1,28 +1,28 @@
 
-  /************************************
-         Heatmap Logic
-  ************************************/
+/************************************
+       Heatmap Logic
+************************************/
 
 
-  // http://www.patrick-wied.at/static/heatmapjs/
-  MAP.heatmapLayer = new HeatmapOverlay({
-    "radius": .5,
-    "maxOpacity": .8,
-    "scaleRadius": true, 
-    "useLocalExtrema": false,
-    latField: 'latitude',
-    lngField: 'longitude',
-  });
+// http://www.patrick-wied.at/static/heatmapjs/
+MAP.heatmapLayer = new HeatmapOverlay({
+  "radius": .5,
+  "maxOpacity": .8,
+  "scaleRadius": true, 
+  "useLocalExtrema": false,
+  latField: 'latitude',
+  lngField: 'longitude',
+});
 
 
-  MAP.addHeatmap = function(data){
+MAP.addHeatmap = function(data){
 
-    var heatmapData = { 
-        max: 1,  // No idea what this does
-        data: data,
-    };
+  var heatmapData = { 
+      max: 1,  // No idea what this does
+      data: data,
+  };
 
-    map.addLayer(MAP.heatmapLayer)
-    MAP.heatmapLayer.setData(heatmapData);
-  }
+  map.addLayer(MAP.heatmapLayer)
+  MAP.heatmapLayer.setData(heatmapData);
+}
 
