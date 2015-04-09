7dc3ee840f31ea1364106898c444ca8a8f613df9
Tommy Hu <tomxhu@gmail.com>
2015-04-09 13:35:21 -0400

    missing parameter errors

--- api/entries.js
+++ api/entries.js
@@ -80,6 +80,10 @@ module.exports = {
         var fidelity = req.query.fidelity;
         var playerIDs = req.query.playerIDs || [];
 
+        if (!game || !area) {
+            return res.status(500).send('Missing parameters');
+        }
+
         return EntryModel.find({game: game, area: area, playerID: {$in: playerIDs}}, function(err, entries) {
 
             console.log("\nGET entries for " + area + " of " + game + " | " + entries.length + " entries.")
@@ -193,6 +197,10 @@ module.exports = {
 
         var promises = [];
 
+        if (!game) {
+            return res.status(500).send('Missing parameters');
+        }
+
         if (!actions) { 
             return EntryModel.find({game: game}).distinct('playerID', function(err, result){
                 if (err) {
@@ -230,7 +238,11 @@ module.exports = {
 
     getActions : function(req, res){
         var game = req.query.game;
-        
+
+        if (!game) {
+            return res.status(500).send('Missing parameters');
+        }
+
         return EntryModel.find({game: game}).distinct('action', function(err, result){
             if (err) {
                 console.log(err);
